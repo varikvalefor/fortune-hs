8975f7c4b02c4bef77537a320723affdc3347796
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-03-06 14:20:32 -0500

    Create fortune.hs
    IT'S ALIVE!

---
+++ fortune.hs
@@ -0,0 +1,15 @@
+import Control.Monad
+import Data.List.Split;
+import System.Environment;
+import System.Random;
+
+nthFortune :: Int -> String -> String;
+nthFortune n f = g !! (mod n $ length g)
+  where g = splitOn "\n%\n" f
+
+rando :: IO [Int];
+rando = newStdGen >>= return . randoms;
+
+main :: IO ();
+main = getArgs >>= readFile . (!!0) >>= \forfil ->
+  rando >>= \numb -> putStrLn $ nthFortune (numb !! 0) forfil;
